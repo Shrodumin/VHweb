5260bc9ee56a180b2ea85a112c44b2e25af468d9
Tomáš Pěnkava <tomaspenkava1@gmail.com>
2025-02-21 04:16:10 +0100

    contacts update

--- frontend/src/pages/Contacts.jsx
+++ frontend/src/pages/Contacts.jsx
@@ -2,7 +2,12 @@ import { useEffect, useState } from "react";
 import ContainerCard from "../components/ContainerCard";
 import NavbarComponent from "./Navbar";
 import styles from "../styles/Contacts.module.css";
-import { Spinner } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
+import { FaHouse } from "react-icons/fa6";
+import { CiMail } from "react-icons/ci";
+import { FaMobile } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { RiBuilding3Fill } from "react-icons/ri";
 
 function Contacts() {
   const [isImageLoading, setIsImageLoading] = useState(true);
@@ -12,8 +17,8 @@ function Contacts() {
     const image = new window.Image();
     image.src = "/intro/intro.jpg";
 
-    image.onload = () => setIsImageLoading(false); // Jakmile se obrázek načte
-    image.onerror = () => setIsImageLoading(false); // I při chybě skryj spinner
+    image.onload = () => setIsImageLoading(false);
+    image.onerror = () => setIsImageLoading(false);
   }, []);
 
   return (
@@ -21,77 +26,75 @@ function Contacts() {
       <NavbarComponent />
       {isImageLoading ? (
         <div className="loading-container" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-          <Spinner animation="border" role="status" style={{ width: "5rem", height: "5rem" }}>
-          </Spinner>
+          <Spinner animation="border" role="status" style={{ width: "5rem", height: "5rem" }} />
         </div>
       ) : (
-        <ContainerCard image="/intro/intro.jpg" className={`d-flex justify-content-between `}
-          style={{ minHeight: "100vh", paddingBottom: "50px" }}>
-          <div className={`d-flex justify-content-between ${styles.cardStyle}`}>
-            <div className={styles.paragraph}>
-              <h1 style={{ textAlign: "left" }}>Kontakty</h1>
+        <ContainerCard image="/intro/intro.jpg" className={styles.container}>
+
+          {/* Dvouvrstvá sekce - Adresa vlevo, Mapa vpravo */}
+          <div className={styles.topSection}>
+            <div className={styles.infoContainer} style={{ padding: "10px"}}>
+              <h1>Kontakty</h1>
               <br />
-              <h4>Mobil</h4>
-              <ul>
-                <li>
-                  <a className={styles.a} href="tel:+420602545077">+420 602 545 077</a><br></br>
-                </li>
-                <li>
-                  <a className={styles.a} href="tel:+420603859971">+420 603 859 971</a>
-                </li>
-              </ul>
-              <h4>Telefon/Fax</h4>
-              <ul>
-                <li>
-                  <a className={styles.a} href="420568620008">420 568 620 008</a>
-                </li>
-              </ul>
-              <h4>E-mail</h4>
-              <ul>
-                <li>
-                  <a className={styles.a} href="mailto:kopulety@mont-stav.cz">kopulety@mont-stav.cz</a><br></br>
-                  <a className={styles.a} href="mailto:kancelar@mont-stav.cz">kancelar@mont-stav.cz</a>
-                </li>
-              </ul>
-              <h4>Adresa</h4>
-              Masarykovo nám. 105 <br />
-              675 71, Náměšť nad Oslavou <br />
               <br />
-              <b>IČ: </b>28263511 <br />
-              <b>DIČ: </b>CZ28263511
+              <p><FaHouse className={styles.icon} /> Masarykovo náměstí 105,<br />675 71, Náměšť nad Oslavou</p>
+              <p><b>IČ:</b> 28263511</p>
+              <p><b>DIČ:</b> CZ28263511</p> <br/>
+              <p>Společnost je zapsána v OR u Krajského soudu v Brně, spisová značka C 57230 </p>
             </div>
+
+            {/* Mapa vedle adresy */}
             <div className={styles.mapContainer}>
               {isIframeLoading && (
-                <div
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "rgba(255, 255, 255, 0.7)",
-                    zIndex: 1,
-                  }}
-                >
-                  <Spinner animation="border" role="status">
-                  </Spinner>
+                <div className={styles.spinnerOverlay}>
+                  <Spinner animation="border" role="status" />
                 </div>
               )}
               <iframe
-                id="mapIframe"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2606.6209692604193!2d16.155093077471516!3d49.207749476056236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470d5fd6782f8d65%3A0x391d47720f4842a!2zTWFzYXJ5a292byBuw6FtLiAxMDUsIDY3NSA3MSBOw6FtxJvFocWlIG5hZCBPc2xhdm91!5e0!3m2!1scs!2scz!4v1738887572680!5m2!1scs!2scz"
                 className={styles.iframe}
-                style={{ border: "0" }}
                 allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
-                onLoad={() => setIsIframeLoading(false)} // Jakmile se iframe načte
+                onLoad={() => setIsIframeLoading(false)}
               />
             </div>
           </div>
+
+          {/* Karty s kontakty */}
+          <div className={styles.cardContainer}>
+            <Card className={styles.contactCard}>
+              <Card.Body>
+                <Card.Title>Kancelář</Card.Title>
+                <Card.Text>
+                  <FaMobile className={styles.icon} /> <a href="tel:+420603859971">+420 603 859 971</a><br />
+                  <BsFillTelephoneFill className={styles.icon} /> <a href="tel:420568620008">420 568 620 008</a><br />
+                  <CiMail className={styles.icon} /> <a href="mailto:kancelar@mont-stav.cz">kancelar@mont-stav.cz</a>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card className={styles.contactCard}>
+              <Card.Body>
+                <Card.Title>Ing. Martin Kopuletý</Card.Title>
+                <Card.Text>
+                  <FaMobile className={styles.icon} /> <a href="tel:+420602545077" >+420 602 545 077</a><br />
+                  <CiMail className={styles.icon} /> <a href="mailto:kopulety@mont-stav.cz">kopulety@mont-stav.cz</a>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card className={styles.contactCard}>
+              <Card.Body>
+                <Card.Title>Skladové prostory</Card.Title>
+                <Card.Text>
+                  <RiBuilding3Fill className={styles.icon}/>Újezd u Rosic ev. č. 26<br />
+                  66484 Újezd u Rosic
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+
         </ContainerCard>
       )}
     </>
