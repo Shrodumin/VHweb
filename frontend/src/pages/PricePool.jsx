63c03694e816daa85def2b0a4936aab4a9a57e38
Tomáš Pěnkava <tomaspenkava1@gmail.com>
2025-02-22 19:01:37 +0100

    pdf updates

--- frontend/src/pages/PricePool.jsx
+++ frontend/src/pages/PricePool.jsx
@@ -1,15 +1,27 @@
+import React from "react";
 import NavbarComponent from "./Navbar";
-import styles from "../styles/PricePool.module.css"; // Přidej styl
+import styles from "../styles/PricePool.module.css";
 
 function PricePool() {
-    return (
-        <>
-            <NavbarComponent />
-            <div className={styles.pdfContainer}>
-                <object className={styles.pdfViewer} data="/2024_Prices.pdf"></object>
-            </div>
-        </>
-    );
+  const pdfUrl = "/2024_Prices.pdf"; // Cesta k souboru
+
+  return (
+    <>
+      <NavbarComponent />
+      <div className={styles.pdfContainer}>
+        {/* Desktop: Použití <object> */}
+        <object className={styles.pdfViewer} data={pdfUrl} type="application/pdf">
+          {/* Pokud prohlížeč nepodporuje <object>, zobrazí se alternativa */}
+          <p>Váš prohlížeč nepodporuje zobrazování PDF. <a href={pdfUrl} download>Stáhněte si PDF zde.</a></p>
+        </object>
+
+        {/* Mobil: Alternativní odkaz ke stažení PDF */}
+        <a href={pdfUrl} className={styles.mobilePdfLink} download>
+          Klikněte zde pro stažení PDF
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default PricePool;
