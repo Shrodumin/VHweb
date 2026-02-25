629a29571b8d63a7e38c1c98d8a31e14e817d5df
Tomáš Pěnkava <tomaspenkava1@gmail.com>
2026-02-25 20:11:51 +0100

    more pdf

--- frontend/src/pages/PricePool.jsx
+++ frontend/src/pages/PricePool.jsx
@@ -4,6 +4,8 @@ import styles from "../styles/PricePool.module.css";
 
 function PricePool() {
   const pdfUrl = "/2026_Prices.pdf"; // Cesta k souboru
+  const norms = "/normy.pdf";
+  const formFile = "/Zakazkovy_formular.pdf";
 
   return (
     <>
@@ -12,7 +14,31 @@ function PricePool() {
         {/* Desktop: Použití <object> */}
         <object className={styles.pdfViewer} data={pdfUrl} type="application/pdf">
           {/* Pokud prohlížeč nepodporuje <object>, zobrazí se alternativa */}
-          <p>Váš prohlížeč nepodporuje zobrazování PDF. <a href={pdfUrl} download>Stáhněte si PDF zde.</a></p>
+          <p>Váš prohlížeč nepodporuje zobrazování PDF. <a href={pdfUrl} download>Ceník ke stažení zde</a></p>
+        </object>
+
+        {/* Mobil: Alternativní odkaz ke stažení PDF */}
+        <a href={pdfUrl} className={styles.mobilePdfLink} download>
+          Klikněte zde pro stažení PDF
+        </a>
+      </div>
+      <div className={styles.pdfContainer}>
+        {/* Desktop: Použití <object> */}
+        <object className={styles.pdfViewer} data={norms} type="application/pdf">
+          {/* Pokud prohlížeč nepodporuje <object>, zobrazí se alternativa */}
+          <p>Váš prohlížeč nepodporuje zobrazování PDF. <a href={norms} download>Normy ke stažení zde</a></p>
+        </object>
+
+        {/* Mobil: Alternativní odkaz ke stažení PDF */}
+        <a href={pdfUrl} className={styles.mobilePdfLink} download>
+          Klikněte zde pro stažení PDF
+        </a>
+      </div>
+      <div className={styles.pdfContainer}>
+        {/* Desktop: Použití <object> */}
+        <object className={styles.pdfViewer} data={formFile} type="application/pdf">
+          {/* Pokud prohlížeč nepodporuje <object>, zobrazí se alternativa */}
+          <p>Váš prohlížeč nepodporuje zobrazování PDF. <a href={formFile} download>Formulář ke stažení zde</a></p>
         </object>
 
         {/* Mobil: Alternativní odkaz ke stažení PDF */}
